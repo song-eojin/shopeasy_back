3e5948c56163dead7e9788491b0d7815cd7422f9
song eojin <feshed528@gmail.com>
2024-08-18 04:30:42 +0900

    Init : 스프링부트 애플리케이션 초기 환경 설정 (Issue #None)
    초기 애플리케이션 환경 설정을 완료했습니다. 프로젝트의 기본 구조와 설정 파일과 의존성을 생성했습니다.

---
+++ src/main/java/com/eojin/shopeasy_back/ShopeasyBackApplication.java
@@ -0,0 +1,13 @@
+package com.eojin.shopeasy_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeasyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeasyBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eojin/shopeasy_back/ShopeasyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eojin.shopeasy_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeasyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
