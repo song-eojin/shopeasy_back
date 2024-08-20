a8b36c0dc81b0430fcf3e82f840f0fbc41e3c606
song eojin <feshed528@gmail.com>
2024-08-21 08:27:04 +0900

    Config : 사용자 - 스프링 시큐리티 및 서버 클라이언트 연동을 위한 설정 추가 (Issue #3)

---
+++ src/main/java/com/eojin/shopeasy_back/common/config/FilterConfig.java
@@ -0,0 +1,17 @@
+package com.eojin.shopeasy_back.common.config;
+
+import com.eojin.shopeasy_back.common.Filter.CorsLoggingFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+    @Bean
+    public FilterRegistrationBean<CorsLoggingFilter> corsLoggingFilter() {
+        FilterRegistrationBean<CorsLoggingFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new CorsLoggingFilter());
+        registrationBean.addUrlPatterns("/*"); // 모든 URL 패턴에 대해 필터 적용
+        return registrationBean;
+    }
+}
