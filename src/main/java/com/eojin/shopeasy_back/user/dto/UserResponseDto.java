7cdf08ef49cd0e8d605d1f5c67fac7cb2dec522c
song eojin <feshed528@gmail.com>
2024-08-22 06:07:25 +0900

    Refactor : 변수 명 변경 (nickname -> realname)

--- src/main/java/com/eojin/shopeasy_back/user/dto/UserResponseDto.java
+++ src/main/java/com/eojin/shopeasy_back/user/dto/UserResponseDto.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 public class UserResponseDto {
     private final Long user_id;
     private final String username;
-    private final String nickname;
+    private final String realname;
     private final String email;
     private final String profile_url;
     private final String password;
@@ -15,7 +15,7 @@ public class UserResponseDto {
     public UserResponseDto(User user) {
         this.user_id = user.getId();
         this.username = user.getUsername();
-        this.nickname = user.getNickname();
+        this.realname = user.getRealname();
         this.email = user.getEmail();
         this.profile_url = user.getProfile_url();
         this.password = user.getPassword();
